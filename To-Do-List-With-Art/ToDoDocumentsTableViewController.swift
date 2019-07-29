3f065f04497e3db1f4de6823c35ffdf992f7d70a
Kristy Lee <kristylee@berkeley.edu>
2019-07-29 02:06:22 -0700

    Attempted segue

--- To-Do-List-With-Art/ToDoDocumentsTableViewController.swift
+++ To-Do-List-With-Art/ToDoDocumentsTableViewController.swift
@@ -136,12 +136,15 @@ class ToDoDocumentsTableViewController: UITableViewController {
     
     
      // MARK: - Navigation
-     /*
+    
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destination.
      // Pass the selected object to the new view controller.
-        if segue.identifier == "Choose To-Do List" {
+        if segue.identifier == "Choose To-Do List", let destination = segue.destination as? ViewController, let index = tableView.indexPathForSelectedRow?.row {
+            destination.tableData = toDoDocuments[index].taskList
+            destination.timeData = toDoDocuments[index].timeList
+            
             
 //            if let button = sender as? UIButton { //because sender is a optional Any type
 //                if let themeName = button.currentTitle {
@@ -154,7 +157,7 @@ class ToDoDocumentsTableViewController: UITableViewController {
 //                }
 //            }
         }
-     }*/
+     }
     
 //    //MARK: Split View Controller
 //    func splitViewController(_ splitViewController: UISplitViewController,
